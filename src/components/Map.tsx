1b558aaa8ac80e98277daf65dabc18b90148cd8a
oussema <oussema9100@gmail.com>
2025-04-20 16:10:04 +0200

    Fix TypeScript definitions for Leaflet and Tailwind config

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -4,7 +4,7 @@ import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 
 // Fix Leaflet default icon issue
-delete L.Icon.Default.prototype._getIconUrl;
+delete (L.Icon.Default.prototype as any)._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
   iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
